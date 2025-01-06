ac53b574f41a1df50836ae7888892d29615164b7
ncguilbeault <ncguilbeault@gmail.com>
2025-01-06 12:26:59 +0000

    Removed hard coded paths

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿var downloadPath = "C:\\Users\\ncgui\\Downloads\\";
-var outputPath = "C:\\Users\\ncgui\\Desktop\\machinelearning\\docs\\examples\\datasets\\fashion-mnist";
+﻿var inputDirectory = Environment.CurrentDirectory;
+var outputDirectory = Path.Combine(inputDirectory, "fashion-mnist");
 
 var trainPrefix = "train";
 var testPrefix = "t10k";
 var imagesSuffix = "-images-idx3-ubyte";
 var labelsSuffix = "-labels-idx1-ubyte";
 
-var trainImagesPath = Path.Combine(downloadPath, trainPrefix + imagesSuffix + "\\" + trainPrefix + imagesSuffix);
-var trainLabelsPath = Path.Combine(downloadPath, trainPrefix + labelsSuffix + "\\" + trainPrefix + labelsSuffix);
+var trainImagesPath = Path.Combine(inputDirectory, trainPrefix + imagesSuffix + "\\" + trainPrefix + imagesSuffix);
+var trainLabelsPath = Path.Combine(inputDirectory, trainPrefix + labelsSuffix + "\\" + trainPrefix + labelsSuffix);
 
-FashionMnistExporter.Export(trainImagesPath, trainLabelsPath, outputPath, trainPrefix);
+FashionMnistExporter.Export(trainImagesPath, trainLabelsPath, outputDirectory, trainPrefix);
 
-var testImagesPath = Path.Combine(downloadPath, testPrefix + imagesSuffix + "\\" + testPrefix + imagesSuffix);
-var testLabelsPath = Path.Combine(downloadPath, testPrefix + labelsSuffix + "\\" + testPrefix + labelsSuffix);
+var testImagesPath = Path.Combine(inputDirectory, testPrefix + imagesSuffix + "\\" + testPrefix + imagesSuffix);
+var testLabelsPath = Path.Combine(inputDirectory, testPrefix + labelsSuffix + "\\" + testPrefix + labelsSuffix);
 
-FashionMnistExporter.Export(testImagesPath, testLabelsPath, outputPath, testPrefix);
+FashionMnistExporter.Export(testImagesPath, testLabelsPath, outputDirectory, testPrefix);
